a5fafb2639a24acc2388c9bb415097a5302c31f7
CND <nhatdungcao@gmail.com>
2026-06-21 21:29:49 +0700

    feat: add campaign DTOs (request + response)

---
+++ AISAM-BE/AISAM.Common/Dtos/Request/UpdateAdCampaignRequest.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AISAM.Common.Dtos.Request
+{
+    public class UpdateAdCampaignRequest
+    {
+        [MaxLength(255)]
+        public string? Name { get; set; }
+
+        public Guid? BrandId { get; set; }
+
+        [MaxLength(255)]
+        public string? AdAccountId { get; set; }
+
+        [MaxLength(100)]
+        public string? Objective { get; set; }
+
+        public decimal? Budget { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
